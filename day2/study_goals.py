c4d141368ba33cf8b400083110b2f3bf92bde5f0
AdrianMorlan <adrianmorlan.dev@gmail.com>
2026-05-26 17:54:15 -0600

    Day 2 study goals system

---
+++ day2/study_goals.py
@@ -0,0 +1,60 @@
+metas = []
+
+
+def mostrar_menu():
+    print("\n=== SISTEMA DE METAS ===")
+    print("1. Agregar meta")
+    print("2. Ver metas")
+    print("3. eliminar meta")
+    print("4. Salir")
+
+
+while True:
+
+    mostrar_menu()
+
+    opcion = input("\nSelecciona una opción: ")
+
+    if opcion == "1":
+
+        meta = input("Escribe una nueva meta: ")
+
+        metas.append(meta)
+
+        print("Meta agregada correctamente.")
+
+    elif opcion == "2":
+
+        print("\n=== TUS METAS ===")
+
+        if len(metas) == 0:
+            print("No tienes metas registradas.")
+
+        else:
+            for meta in metas:
+                print(f"- {meta}")
+
+    elif opcion == "3":
+
+        print("\n=== ELIMINAR META ===")
+
+        Bmeta = input("Escribe qué meta quieres eliminar: ")
+
+        if Bmeta in metas:
+
+            metas.remove(Bmeta)
+
+            print("Meta eliminada correctamente.")
+
+
+        else:
+
+            print("Esa meta no existe.")
+
+    elif opcion == "4":
+
+        print("Saliendo del sistema...")
+        break
+
+    else:
+        print("Opción inválida.")
